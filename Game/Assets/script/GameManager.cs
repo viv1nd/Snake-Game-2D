f8d11f0efd92095e35b1d6f418eb0e26c2bbbe44
P D Vivekanand <vivekanandpd5@gmail.com>
2023-07-14 08:34:29 +0530

    Added the game

---
+++ Game/Assets/script/GameManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+    public List<Eatable> onMapObjects;
+
+    public Transform poison;
+    public Transform food;
+    //public Transform powerup;
+    [SerializeField] [Range(0f,10f)] private float speed;
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public Text scoreText;
+    
+    public float gametime = 0;
+    public Text timerText;
+    private int score = 0;
+
+    [SerializeField] private float startSpeed = 1.1f;
+    [SerializeField] private float powerupSpeed = 0.7f;
+
+
+    private void Start()
+    {
+        scoreText.text = score.ToString();
+        speed = startSpeed;
+
+
+    }
+
+    private void Update()
+    {
+        gametime = gametime + Time.deltaTime;
+        timerText.text = Mathf.RoundToInt(gametime).ToString();
+    }
+
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+    }
+
+    public void RemoveScore()
+    {
+        if (score < 1)
+        {
+            return;
+        }
+        score--;
+        scoreText.text = score.ToString();
+    }
+
+    public void speedup()
+    {
+        speed = startSpeed;
+    }
+
+    public void SpeedDown()
+    {
+        speed = powerupSpeed;
+    }
+
+    public void Sheild()
+    {
+        SpeedDown();
+
+    }
+
+    public Vector2 GenerateSpawnPoint(BoxCollider2D gridArea)
+    {
+        Bounds bounds = gridArea.bounds;
+        Vector2 position;
+        position.x = Random.Range(bounds.min.x, bounds.max.x);
+        position.y = Random.Range(bounds.min.y, bounds.max.y);
+        if (!ValidatePosition(position))
+             position = GenerateSpawnPoint(gridArea);
+        return position;
+            
+    }
+
+    private bool ValidatePosition(Vector2 aPosition)
+    {
+        Debug.Log("validating points"); 
+        foreach (Eatable mapObject in onMapObjects)
+        {
+
+            Vector2 bPosition = new Vector2(mapObject.transform.position.x, mapObject.transform.position.y);
+            Debug.Log("validating point a:" + aPosition + " and pointb: " + bPosition);
+            float distance = CheckDistance(aPosition, bPosition);
+            Debug.Log(distance);
+            if(distance < 6.5f)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private float CheckDistance(Vector2 a, Vector2 b)
+    {
+        return Vector2.Distance(a, b);
+    }
+
+    public void GenerateFood()
+    {
+
+    }
+
+}
---
+++ Game/Assets/script/Snake.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+
+public class Snake : MonoBehaviour
+{
+    private Vector3 _direction = Vector3.down;
+
+    public List<Transform> _snakeparts = new List<Transform>();
+
+    
+    public Transform snakePrefab;
+
+    private int _intialsnakesize = 0;
+
+    [SerializeField] private Transform tailParent = null;
+
+    float currentTime = 0;
+    bool spacePressed = false;
+
+    private void Start()
+    {
+        generateSnake();
+    }
+
+    private void Update()
+    {
+        if (spacePressed)
+        {
+            if (GameManager.Instance.gametime - currentTime > 5f)
+            {
+                GameManager.Instance.SpeedDown();
+                spacePressed = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && _direction.x != 0)
+        {
+            Vector2 vec;
+            vec.x = 0;
+            vec.y = 1;
+            _direction = vec;
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else if ( Input.GetKeyDown(KeyCode.S) && _direction.x != 0)
+        {
+            Vector2 vec;
+            vec.x = 0;
+            vec.y = -1;
+            _direction = vec;
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && _direction.y != 0)
+        {
+            Vector2 vec;
+            vec.x = -1;
+            vec.y = 0;
+            _direction = vec;
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && _direction.y != 0)
+        {
+            Vector2 vec;
+            vec.x = 1;
+            vec.y = 0;
+            _direction = vec;
+            transform.rotation = Quaternion.Euler(0, 0, -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameManager.Instance.speedup();
+            currentTime = GameManager.Instance.gametime;
+            spacePressed = true;
+        }
+
+
+        
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(transform.position.x <= -30 || transform.position.x >= 30)
+        {
+            Vector2 vec;
+            float x = transform.position.x;
+            x = x/30 * 29 * -1;
+            float y = transform.position.y;
+            vec.x = x;
+            vec.y = y;
+            transform.position = vec;
+        }
+        if (transform.position.y <= -15 || transform.position.y >= 15)
+        {
+            Vector2 vec;
+            float y = transform.position.y;
+            float x = transform.position.x;
+            y = y / 15 * 14 * -1;
+            vec.y = y;
+            vec.x = x;
+            transform.position = vec;
+        }
+
+        if (_snakeparts.Count != 0)
+            _snakeparts[0].position = transform.position - _direction * 1.1f;
+
+        for (int i = _snakeparts.Count -1; i > 0; i--)
+        {
+            if(_snakeparts[i]!= null)
+                _snakeparts[i].position = _snakeparts[i - 1].position;
+        }
+
+        this.transform.position = new Vector3(
+            transform.position.x + _direction.x * GameManager.Instance.GetSpeed(),
+            transform.position.y + _direction.y * GameManager.Instance.GetSpeed(),
+            0.0f
+        );
+    }
+
+
+    private void Grow()
+    {
+        Transform tail = Instantiate(this.snakePrefab);
+        if(_snakeparts.Count != 0)
+            tail.position = _snakeparts[_snakeparts.Count - 1].position;
+        else
+            tail.position = new Vector3(
+            transform.position.x - _direction.x,
+            transform.position.y - _direction.y,
+            0.0f
+        );
+        _snakeparts.Add(tail);
+    }
+
+
+    public void Poison()
+    {
+        if(_snakeparts.Count < 1)
+        {
+            ResetState();
+            return;
+        }
+        Transform tail = _snakeparts[_snakeparts.Count - 1];
+        if ( tail != null)
+        {
+            _snakeparts.Remove(tail);
+            Destroy(tail.gameObject);
+        }
+    }
+
+    private void generateSnake()
+    {
+
+        for (int i = 0; i < _intialsnakesize; i++)
+        {
+            _snakeparts.Add(Instantiate(snakePrefab,tailParent));
+        }
+    }
+
+    private void ResetState()
+    {
+        for(int i = 0; i < _snakeparts.Count; i++)
+        {
+            Destroy(_snakeparts[i].gameObject);
+        }
+
+        _snakeparts.Clear();
+        //_snakeparts.Add(transform);
+
+        generateSnake();
+        transform.position = Vector3.zero;
+        GameManager.Instance.ResetScore();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.GetComponent<Food>())
+        {
+            Grow();
+            collision.GetComponent<Food>().OnDigest();
+        }
+        else if (collision.GetComponent<Poison>())
+        {
+            Poison();
+            collision.GetComponent<Poison>().OnDigest();
+        }
+        if (collision.CompareTag("Tail"))
+        {
+            ResetState();
+        }
+        if (collision.CompareTag("Shield"))
+        {
+            collision.GetComponent<Shieldpowerup>().OnDigest();
+        }
+
+        //else if (collision.tag == ("Wall"))
+        {
+            //ResetState();
+        }
+    }
+
+}
